bb06626db75672288931b501c242f8deb68e033e
stbn <stbnrivas@gmail.com>
2020-02-18 10:35:03 +0100

    add ALLOW receive remote request for

--- rails-bot/config/environments/development.rb
+++ rails-bot/config/environments/development.rb
@@ -49,4 +49,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  ## TODO: don't work with ENV you should set with your own
+  config.hosts << "subdomain.domain.io"
 end
